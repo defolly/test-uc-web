5f94e37c07fed24447e14e7c90303fec6c0b648c
denny <wu.df@belle.com.cn>
2022-05-23 16:40:31 +0800

    docs

---
+++ docs/p__role.f92185e0.async.js
@@ -0,0 +1,25 @@
+(window['webpackJsonp_lesoon-permission-web'] =
+  window['webpackJsonp_lesoon-permission-web'] || []).push([
+  [1],
+  {
+    muQm: function (e, s, n) {
+      'use strict';
+      n.r(s);
+      var o = n('q1tI'),
+        p = n('nKUr');
+      s['default'] = () => (
+        Object(o['useEffect'])(() => {
+          console.log(
+            '\u5f02\u5e38\u51fa\u73b0\u4e86\uff0c\u53d8\u91cf\u6ca1\u6709\u5b9a\u4e49',
+            lsApps,
+          );
+        }, []),
+        Object(p['jsxs'])('a', {
+          href: './excel/\u7528\u6237\u7ba1\u7406\u5bfc\u5165\u6a21\u677f.xlsx',
+          style: { padding: '50px' },
+          children: ['\u4e0b\u8f7d\u7528\u6237\u5bfc\u5165\u6a21\u677f', ' '],
+        })
+      );
+    },
+  },
+]);
